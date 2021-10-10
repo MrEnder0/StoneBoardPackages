937cfd73c12d3b0ad73a4c9a3a07729dc200a39b
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2021-10-09 19:05:44 -0500

    home
    home

---
+++ home.py
@@ -0,0 +1,30 @@
+from config import *
+import pygame
+import time
+
+done = False
+
+def startHome():
+  pygame.quit()
+  pygame.init()
+  pygame.display.set_caption('StoneBoard Home')
+  (width, height) = (1500, 1000)
+  screen = pygame.display.set_mode((width, height))
+  home_background_colour = (41,60,57)
+  screen.fill(home_background_colour)
+  print("Launched Home")
+  done = False
+
+  #Test if launcher opened before
+
+  if Experintal == True:
+    stoneBoard_experimentalRelease = pygame.image.load('artAssets/stoneBoard_experimentalRelease.png').convert_alpha()
+    stoneBoard_experimentalRelease = pygame.transform.scale(stoneBoard_experimentalRelease, (120, 120))
+    screen.blit(stoneBoard_experimentalRelease,(0,-35))
+
+  stoneBoard_logo = pygame.image.load('artAssets/stoneBoard_logo.png').convert_alpha()
+  stoneBoard_logo = pygame.transform.scale(stoneBoard_logo, (250, 250))
+  screen.blit(stoneBoard_logo,(1240,-105))
+  pygame.display.flip()
+
+  #Add It To Load New Stuff
