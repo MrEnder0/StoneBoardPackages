e765e3e730e4601da2693ef7e50cdcebebc29a75
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2021-10-10 15:54:09 -0500

    Fixed the file location
    changed experimental to stable

--- home.py
+++ home.py
@@ -14,15 +14,8 @@ def startHome():
   screen.fill(home_background_colour)
   print("Launched Home")
   done = False
-
-  #Test if launcher opened before
-
-  if Experintal == True:
-    stoneBoard_experimentalRelease = pygame.image.load('experimental/StoneBoardPackagesexperimental/assets/stoneBoard_experimentalRelease.png').convert_alpha()
-    stoneBoard_experimentalRelease = pygame.transform.scale(stoneBoard_experimentalRelease, (120, 120))
-    screen.blit(stoneBoard_experimentalRelease,(0,-35))
-
-  stoneBoard_logo = pygame.image.load('experimental/StoneBoardPackagesexperimental/assets/stoneBoard_logo.png').convert_alpha()
+  
+  stoneBoard_logo = pygame.image.load('stable/StoneBoardPackagesstable/assets/stoneBoard_logo.png').convert_alpha()
   stoneBoard_logo = pygame.transform.scale(stoneBoard_logo, (250, 250))
   screen.blit(stoneBoard_logo,(1240,-105))
   pygame.display.flip()
