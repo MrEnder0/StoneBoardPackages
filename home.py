90abde6831d076cef5e6db12a5fe4c34394fba60
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2021-10-10 22:50:28 -0500

    Better

--- home.py
+++ home.py
@@ -7,8 +7,8 @@ def startHome():
   pygame.init()
   pygame.display.set_caption('StoneBoard Home')
   (width, height) = (1500, 1000)
-  screen = pygame.display.set_mode((width, height))
-  home_background_colour = (41,60,57)
+  screen = pygame.display.set_mode((width, height), pygame.NOFRAME)
+  home_background_colour = (250,250,250)
   screen.fill(home_background_colour)
   print("Launched Home")
   run = True
@@ -19,14 +19,14 @@ def startHome():
     screen.blit(stoneBoard_experimentalRelease,(0,-35))
 
   stoneBoard_logo = pygame.image.load('experimental/StoneBoardPackagesexperimental/assets/stoneBoard_logo.png').convert_alpha()
-  done = False
+  stoneBoard_logo = pygame.transform.scale(stoneBoard_logo, (288, 28))
+  screen.blit(stoneBoard_logo,(1200,10))
   
-  stoneBoard_logo = pygame.image.load('stable/StoneBoardPackagesstable/assets/stoneBoard_logo.png').convert_alpha()
-  stoneBoard_logo = pygame.transform.scale(stoneBoard_logo, (250, 250))
-  screen.blit(stoneBoard_logo,(1240,-105))
-  pygame.display.flip()
+  stoneBoard_logo_rectangle = pygame.image.load('experimental/StoneBoardPackagesexperimental/assets/stoneBoard_logo_rectangle.png').convert_alpha()
+  stoneBoard_logo_rectangle = pygame.transform.scale(stoneBoard_logo_rectangle, (400, 200))
+  screen.blit(stoneBoard_logo_rectangle,(1180,-130))
 
-  #Add It To Load New Stuff
+  #Buttons
 
   button_img = pygame.image.load('experimental/StoneBoardPackagesexperimental/assets/bottom_bar.png').convert_alpha()
 
@@ -42,12 +42,13 @@ def startHome():
       screen.blit(self.image, (self.rect.x, self.rect.y))
 
 
-  start_button = Button(0, 920, button_img, 1)
+  start_button = Button(-5, 920, button_img, 0.266)
 
   while run:
     screen.fill(home_background_colour)
+    screen.blit(stoneBoard_logo_rectangle,(1180,-135))
     screen.blit(stoneBoard_experimentalRelease,(0,-35))
-    screen.blit(stoneBoard_logo,(1240,-105))
+    screen.blit(stoneBoard_logo,(1200,10))
     start_button.draw()
     pygame.display.flip()
 
