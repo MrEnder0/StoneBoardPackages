90ef0e1fb807b67d7f1dd015119eced6ec7074cc
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2021-10-17 20:08:47 -0500

    Cave Man
    Added secret cave man sound with a randomizer for different sounds to play.

--- soundManager.py
+++ soundManager.py
@@ -1,11 +1,12 @@
 from config import *
 import pygame
+import random
 
 def music():
     pygame.mixer.init()
     path = 'experimental/StoneBoardPackagesexperimental/assets/sounds/'
-    sound = pygame.mixer.Sound(path + "591665__erokia__sunburst.mp3")
-    sound.set_volume(0.1)
+    music = pygame.mixer.Sound(path + "591665__erokia__sunburst.mp3")
+    music.set_volume(0.1)
 
     if PlayMusic:
         pygame.mixer.music.load(path + "591665__erokia__sunburst.mp3") 
@@ -18,3 +19,17 @@ def clickSound():
     sound.set_volume(0.3)
     pygame.mixer.music.load(path + "uiClick.mp3") 
     pygame.mixer.music.play(1,0.0)
+
+def caveManSound():
+    pygame.mixer.init()
+    randomGrunt = random.randint(1,3)
+    path = 'experimental/StoneBoardPackagesexperimental/assets/sounds/'
+    if randomGrunt == 1:
+        pygame.mixer.music.load(path + "cave_man1.mp3") 
+        pygame.mixer.music.play(1,0.0)
+    elif randomGrunt == 2:
+        pygame.mixer.music.load(path + "cave_man2.mp3") 
+        pygame.mixer.music.play(1,0.0)
+    elif randomGrunt == 3:
+        pygame.mixer.music.load(path + "cave_man3.mp3") 
+        pygame.mixer.music.play(1,0.0)
