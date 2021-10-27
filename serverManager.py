278dd201cf4b356bf3ab0b1c10217f195ed6d3b0
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2021-10-26 21:47:24 -0500

    Load server func progress
    working on function to load a servers data file

--- serverManager.py
+++ serverManager.py
@@ -6,21 +6,38 @@ import uuid
 import os
 
 def createServer():
-    #delete old failed servers without error
+    #delete failed server without error
     try:
         shutil.rmtree("storage/servers/createdServer")
-        print('Deleting old failed server!')
+        print("Deleting old failed server!")
     except:
         pass
     
     #create basic servcer file structure
-    os.mkdir('storage/servers/createdServer')
-    userFilePath = 'storage/user.txt'
-    with open(userFilePath, 'r') as userId:
+    os.mkdir("storage/servers/createdServer")
+    userFilePath = "storage/user.txt"
+    with open(userFilePath, "r") as userId:
         userUuid = userId.read()
-    with open('storage/servers/createdServer/serverOwner.txt', 'w') as serverOwnerId:
+    with open("storage/servers/createdServer/serverOwner.txt", "w") as serverOwnerId:
         serverOwnerId.write(userUuid)
-        
+
+    #generate empty save file
+    with open("storage/servers/createdServer/serverData.txt", "w") as serverData:
+        serverData.write("#save data\nhi")
+    
+    #rename to randomly generated uuid
     serveruuid = uuid.uuid4()
     serverPath = "storage/servers/" + str(serveruuid)
     os.renames("storage/servers/createdServer", serverPath)
+
+def loadServer():
+    serversFolder = "storage/servers/createdServer/"
+    serverName = "0"
+    openedServer = serverFolder + serverPath
+    serverDataFile = openedServer + "/serverData.txt"
+    
+    f = open(serverDataFile, "r")
+    line = f.readline()
+    print(line)
+    f.close()
+    
