7bb06ce91a36daf4a490c5ebc55fd835fd0ef902
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2021-10-26 21:16:42 -0500

    Owner uuid
    added a owner uuid file so a user can find original creator of server

--- serverManager.py
+++ serverManager.py
@@ -8,19 +8,19 @@ import os
 def createServer():
     #delete old failed servers without error
     try:
-        print('Deleting old failed server!')
         shutil.rmtree("storage/servers/createdServer")
+        print('Deleting old failed server!')
     except:
         pass
     
     #create basic servcer file structure
     os.mkdir('storage/servers/createdServer')
-    serveruuid = uuid.uuid4()
-    serverPath = "storage/servers/" + str(serveruuid)
-    os.renames("storage/servers/createdServer", serverPath)
-    
     userFilePath = 'storage/user.txt'
     with open(userFilePath, 'r') as userId:
         userUuid = userId.read()
     with open('storage/servers/createdServer/serverOwner.txt', 'w') as serverOwnerId:
         serverOwnerId.write(userUuid)
+        
+    serveruuid = uuid.uuid4()
+    serverPath = "storage/servers/" + str(serveruuid)
+    os.renames("storage/servers/createdServer", serverPath)
