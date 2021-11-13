20b339e9fe1161fbcaf2bacf6b0dc12441d0bbf3
Mr.Ender <72358826+MrEnder0@users.noreply.github.com>
2021-11-13 11:19:20 -0800

    Update home.py
    added visual stuff so its actually usable

--- home.py
+++ home.py
@@ -5,7 +5,9 @@ import pygame
 import time
 import uuid
 import os
+
 import webbrowser
+
 def startHome():
   pygame.quit()
   pygame.init()
@@ -14,14 +16,15 @@ def startHome():
   screen = pygame.display.set_mode((width, height), pygame.NOFRAME)
   home_background_colour = (250,250,250)
   screen.fill(home_background_colour)
-  print("Launched Home")
   userFilePath = "storage/user.txt"
   menuSelectLine = 1
   run = True
   menu = 0
+  print("Launched Home")
   #webbrowser.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', new=2)
   
   music()
+  
   #Test if existing user
   oldUser = os.path.isfile("storage/user.txt")
   if oldUser:
@@ -33,17 +36,20 @@ def startHome():
     useruuid = uuid.uuid4()
     with open(userFilePath, "w") as user:
       user.write(str(useruuid))
+      
   #Experimental indicator
   if Experintal == True:
     stoneBoard_experimentalRelease = pygame.image.load("experimental/StoneBoardPackagesexperimental/assets/stoneBoard_experimentalRelease.png").convert_alpha()
     stoneBoard_experimentalRelease = pygame.transform.scale(stoneBoard_experimentalRelease, (120, 120))
+    
   stoneBoard_logo = pygame.image.load("experimental/StoneBoardPackagesexperimental/assets/stoneBoard_logo.png").convert_alpha()
   stoneBoard_logo = pygame.transform.scale(stoneBoard_logo, (288, 28))
-  
   stoneBoard_logo_rectangle = pygame.image.load('experimental/StoneBoardPackagesexperimental/assets/stoneBoard_logo_rectangle.png').convert_alpha()
   stoneBoard_logo_rectangle = pygame.transform.scale(stoneBoard_logo_rectangle, (400, 200))
   stoneBoard_server_ui = pygame.image.load('experimental/StoneBoardPackagesexperimental/assets/serverUi.png').convert_alpha()
   stoneBoard_server_ui = pygame.transform.scale(stoneBoard_server_ui, (800, 500))
+  menuSelectionBox = pygame.image.load('experimental/StoneBoardPackagesexperimental/assets/menuSelectionBox.png').convert_alpha()
+  menuSelectionBox = pygame.transform.scale(menuSelectionBox, (700, 40))
   exitButton = pygame.image.load("experimental/StoneBoardPackagesexperimental/assets/exit.png").convert_alpha()
   joinButton = pygame.image.load("experimental/StoneBoardPackagesexperimental/assets/join.png").convert_alpha()
   acceptButton = pygame.image.load("experimental/StoneBoardPackagesexperimental/assets/accept.png").convert_alpha()
@@ -51,6 +57,7 @@ def startHome():
   bottomBar = pygame.image.load("experimental/StoneBoardPackagesexperimental/assets/bottom_bar.png").convert_alpha()
   newUserScreen = pygame.image.load("experimental/StoneBoardPackagesexperimental/assets/newUserScreen.png").convert_alpha()
   minimizeButton = pygame.image.load("experimental/StoneBoardPackagesexperimental/assets/menu_minimize.png").convert_alpha()
+  
   class Button():
     def __init__(self, x, y, image, scale):
       width = image.get_width()
@@ -71,6 +78,7 @@ def startHome():
       
       screen.blit(self.image, (self.rect.x, self.rect.y))
       return action
+    
   serverUiExit = Button(1060, 626, minimizeButton, 0.13)
   serverUiOpen = Button(10, 928, joinButton, 0.13)
   serverUiCreate = Button(370, 626, joinButton, 0.13)
@@ -78,12 +86,14 @@ def startHome():
   newUserScreen = Button(466, 60, newUserScreen, 1)
   bottomBar = Button(-5, 920, bottomBar, 0.266)
   exitButton = Button(1420, 928, exitButton, 0.13)
+  
   while run:
     screen.fill(home_background_colour)
     screen.blit(stoneBoard_experimentalRelease,(0,-35))
     screen.blit(stoneBoard_logo_rectangle,(1180,-135))
     screen.blit(stoneBoard_logo,(1200,10))
     bottomBar.draw()
+    
     if exitButton.draw():
         clickSound()
         time.sleep(0.6)
@@ -96,6 +106,7 @@ def startHome():
       lineDistance = 0
       lineCount = 0
       screen.blit(stoneBoard_server_ui,(350,200))
+      
       with open('storage/serversList.txt', 'r') as serversList:
         for line in serversList:
            lineDistance += 40
@@ -103,6 +114,8 @@ def startHome():
            lineText = line
            lineText = lineText[:-1]
            screen.blit(boldFont.render(str(lineText), 0, (200, 200, 240)), (450, 260 + lineDistance))
+
+      screen.blit(menuSelectionBox,(375,250 + menuSelectLine * 40))
       key_input = pygame.key.get_pressed()   
       if key_input[pygame.K_UP]:
         if not menuSelectLine == 1:
